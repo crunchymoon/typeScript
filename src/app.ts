285f6acac099713ac32ee137f669e587c79cb322
Denis <sldenys96@hotmail.com>
2022-03-05 12:18:46 +0100

    classes learnt

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        toFrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
+
